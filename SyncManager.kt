bee73f53edd6500339706e731d4a169b8c26d1f7
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-30 19:42:07 +0100

    Create SyncManager.kt

---
+++ SyncManager.kt
@@ -0,0 +1,6 @@
+package com.example.smarttask.sync
+
+object SyncManager {
+    // Placeholder: implement Firestore / REST sync logic here
+    fun startSync() { /* schedule sync with WorkManager */ }
+}
