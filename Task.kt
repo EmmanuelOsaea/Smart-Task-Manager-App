fa0d64dfb3aaa30b756eb669ca9124980c7881b7
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-31 10:13:08 +0100

    Update Task.kt

--- Task.kt
+++ Task.kt
@@ -7,6 +7,7 @@ import androidx.room.PrimaryKey
 data class Task(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
-    val description: String,
+    val description: String?,
+    val dueDate: Long,
     val isCompleted: Boolean = false
 )
