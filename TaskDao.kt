4f57cd4f7384905b4f914d8309e64068fcf85ac3
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-31 10:23:02 +0100

    Update TaskDao.kt

--- TaskDao.kt
+++ TaskDao.kt
@@ -2,18 +2,21 @@ package com.example.taskmanager.data
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import com.smarttaskmanager.model.Task
 
 @Dao
 interface TaskDao {
-    @Query("SELECT * FROM tasks")
-    fun getAllTasks(): LiveData<List<Task>>
-
+    
+@Query("SELECT * FROM tasks ORDER BY dueDate ASC")
+    fun getAllTasks(): Flow<List<Task>>
+    
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
 
-    @Delete
-    suspend fun delete(task: Task)
+    @Query("DELETE FROM tasks WHERE id = :id")
+    suspend fun deleteTask(id: Int)
 
     @Update
-    suspend fun update(task: Task)
+    suspend fun updateTask(task: Task)
 }
