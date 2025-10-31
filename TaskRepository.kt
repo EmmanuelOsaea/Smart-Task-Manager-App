bbed1f5e7bad1345c384053f153f2b2b4d6803f2
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-31 10:32:36 +0100

    Update TaskRepository.kt

--- TaskRepository.kt
+++ TaskRepository.kt
@@ -1,8 +1,17 @@
-package com.example.taskmanager.data
+package com.example.smarttaskmanager.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.smarttaskmanager.model.Task
 
 class TaskRepository(private val dao: TaskDao) {
     val allTasks = dao.getAllTasks()
     suspend fun insert(task: Task) = dao.insert(task)
     suspend fun delete(task: Task) = dao.delete(task)
     suspend fun update(task: Task) = dao.update(task)
+}
+
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
 }
