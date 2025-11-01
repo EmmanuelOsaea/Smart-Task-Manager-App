dd815dace32aba82531b6768b6e8ef68902540dc
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-11-01 12:54:22 +0100

    Create ReminderReceiver.kt

---
+++ ReminderReceiver.kt
@@ -0,0 +1,24 @@
+package com.example.smarttaskmanager
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class ReminderReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val taskTitle = intent.getStringExtra("TASK_TITLE") ?: "Task Reminder"
+
+        val builder = NotificationCompat.Builder(context, "task_channel")
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("Task Reminder")
+            .setContentText("Don’t forget: $taskTitle")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(System.currentTimeMillis().toInt(), builder.build())
+        }
+    }
+}
